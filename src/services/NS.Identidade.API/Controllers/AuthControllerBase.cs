1edb75ab30970b4b8c2d5ad8792cc9a17282fbce
pedro.mapelli <pedro.m@lyncas.net>
2022-04-30 15:52:55 -0300

    feat: Response customizado

---
+++ src/services/NS.Identidade.API/Controllers/AuthControllerBase.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using System.Security.Claims;
+using NS.Identidade.API.Models;
+using Microsoft.Extensions.Options;
+using NS.Identidade.API.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace NS.Identidade.API.Controllers;
+
+public abstract class AuthControllerBase : ControllerBase
+{
+    private readonly AppSettings _appSettings;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    protected AuthControllerBase(IOptions<AppSettings> appSettings,
+        UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+        _appSettings = appSettings.Value;
+    }
+
+    protected async Task<UsuarioRespostaLogin> GerarJwt(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var claims = await _userManager.GetClaimsAsync(user);
+
+        var identityClaims = await ObterClaimsUsuario(claims, user);
+        var encodedToken = CodificarToken(identityClaims);
+
+        return ObterRespostaToken(encodedToken, user, claims);
+    }
+
+    private async Task<ClaimsIdentity> ObterClaimsUsuario(ICollection<Claim> claims, IdentityUser user)
+    {
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+        foreach (var userRole in userRoles)
+        {
+            claims.Add(new Claim("role", userRole));
+        }
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        return identityClaims;
+    }
+
+    private string CodificarToken(ClaimsIdentity identityClaims)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.ValidoEm,
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        return tokenHandler.WriteToken(token);
+    }
+
+    private UsuarioRespostaLogin ObterRespostaToken(string encodedToken, IdentityUser user, IEnumerable<Claim> claims)
+    {
+        return new UsuarioRespostaLogin
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+            UsuarioToken = new UsuarioToken
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+            }
+        };
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+}
