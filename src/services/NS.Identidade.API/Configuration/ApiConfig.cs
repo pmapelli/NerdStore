0dc03316466aabc4244b016af778c4e26c279243
pedro.mapelli <pedro.m@lyncas.net>
2022-04-30 16:16:16 -0300

    feat: Organizando Program.cs

---
+++ src/services/NS.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace NS.Identidade.API.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseHttpsRedirection();
+
+        app.UseIdentityConfiguration();
+
+        return app;
+    }
+}
--- src/services/NS.Identidade.API/Program.cs
+++ src/services/NS.Identidade.API/Program.cs
@@ -1,11 +1,6 @@
-using System.Text;
 using NS.Identidade.API.Data;
-using Microsoft.OpenApi.Models;
-using NS.Identidade.API.Extensions;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using NS.Identidade.API.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,63 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddErrorDescriber<IdentityMensagensPortugues>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+builder.Services.AddIdentityConfiguration(builder.Configuration);
 
-var appSettingsSection = builder.Configuration.GetSection("AppSettings");
-builder.Services.Configure<AppSettings>(appSettingsSection);
+builder.Services.AddApiConfiguration();
 
-var appSettings = appSettingsSection.Get<AppSettings>();
-var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(bearerOptions =>
-{
-    bearerOptions.RequireHttpsMetadata = true;
-    bearerOptions.SaveToken = true;
-    bearerOptions.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = appSettings.ValidoEm,
-        ValidIssuer = appSettings.Emissor
-    };
-});
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Title = "NerdStore Identity API",
-        Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
-        Contact = new OpenApiContact() { Name = "Pedro Mapelli", Email = "pmapelli@hotmail.com" },
-        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
-    });
-
-});
+builder.Services.AddSwaggerConfiguration();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    });
-}
+app.UseSwaggerConfiguration();
+
+app.UseApiConfiguration(app.Environment);
 
 app.UseHttpsRedirection();
 
