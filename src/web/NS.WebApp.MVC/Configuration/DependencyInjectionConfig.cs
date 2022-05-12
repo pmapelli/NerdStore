d46589f80d790a91589c5d143cec493132c6b712
pedro.mapelli <pedro.m@lyncas.net>
2022-05-12 07:25:01 -0300

    feat: Implementando Refit

--- src/web/NS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -10,8 +10,17 @@ public static class DependencyInjectionConfig
     {
         services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
         services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
         services.AddHttpClient<ICatalogoService, CatalogoService>()
             .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
+        //services.AddHttpClient("Refit",
+        //        options =>
+        //        {
+        //            options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+        //        })
+        //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+        //    .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<IUser, AspNetUser>();
