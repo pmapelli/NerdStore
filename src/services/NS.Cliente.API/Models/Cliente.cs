bb2aeb798805d00d80c361bd63ff217fce3f6748
pedro.mapelli <pedro.m@lyncas.net>
2022-05-13 06:47:02 -0300

    feat: Objetos de valor e validações no contexto de Clientes

--- src/services/NS.Cliente.API/Models/Cliente.cs
+++ src/services/NS.Cliente.API/Models/Cliente.cs
@@ -5,8 +5,8 @@ namespace NS.Cliente.API.Models;
 public class Cliente : Entity, IAggregateRoot
 {
     public string Nome { get; private set; }
-    public string Email { get; private set; }
-    public string Cpf { get; private set; }
+    public Email Email { get; private set; }
+    public Cpf Cpf { get; private set; }
     public bool Excluido { get; private set; }
     public Endereco Endereco { get; private set; }
 
@@ -17,14 +17,14 @@ public class Cliente : Entity, IAggregateRoot
     {
         Id = id;
         Nome = nome;
-        Email = email;
-        Cpf = cpf;
+        Email = new Email(email);
+        Cpf = new Cpf(cpf);
         Excluido = false;
     }
 
     public void TrocarEmail(string email)
     {
-        Email = email;
+        Email = new Email(email);
     }
 
     public void AtribuirEndereco(Endereco endereco)
